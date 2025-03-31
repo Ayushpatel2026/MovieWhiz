19e5ba8882e2fce24b441fcaf0b962813019697b
Gary Qin <garyzhqin@gmail.com>
2025-03-30 21:27:27 -0400

    feat: frontend auth

---
+++ frontend/app/index.tsx
@@ -0,0 +1,107 @@
+import { useState } from "react";
+import {
+  ActivityIndicator,
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "@react-native-firebase/auth";
+import { FirebaseError } from "firebase/app";
+
+export default function Index() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const auth = getAuth();
+  const signUp = async () => {
+    setLoading(true);
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      alert("Check your email for verification");
+    } catch (e: any) {
+      const err = e as FirebaseError;
+      alert("Registration failed: " + err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const signIn = async () => {
+    setLoading(true);
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      alert("Check your email for verification");
+    } catch (e: any) {
+      const err = e as FirebaseError;
+      alert("Sign in failed: " + err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <KeyboardAvoidingView behavior="padding">
+        <TextInput
+          style={styles.input}
+          value={email}
+          onChangeText={setEmail}
+          autoCapitalize="none"
+          keyboardType="email-address"
+          placeholder="Email"
+        />
+        <TextInput
+          style={styles.input}
+          value={password}
+          onChangeText={setPassword}
+          autoCapitalize="none"
+          placeholder="Password"
+          secureTextEntry
+        />
+        {loading ? (
+          <ActivityIndicator size={"small"} style={{ margin: 28 }} />
+        ) : (
+          <>
+            {" "}
+            <TouchableOpacity style={styles.button} onPress={signIn}>
+              <Text>Log In</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={signUp}>
+              <Text>Create Account</Text>
+            </TouchableOpacity>
+          </>
+        )}
+      </KeyboardAvoidingView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 20,
+    flex: 1,
+    justifyContent: "center",
+  },
+  input: {
+    marginVertical: 4,
+    height: 50,
+    borderWidth: 1,
+    borderRadius: 4,
+    padding: 10,
+    backgroundColor: "#ffffff",
+  },
+  button: {
+    padding: 10,
+    marginTop: 5,
+    backgroundColor: "#dddddd",
+    alignItems: "center",
+  },
+});
