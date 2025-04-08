ce4ee913c81c082b76d2dc50c5188d51ba6ad032
Ayushpatel2026 <ayushpatel.2026@gmail.com>
2025-04-08 10:02:20 -0400

    fix: deployment

--- backend/src/config/firebaseConfig.ts
+++ backend/src/config/firebaseConfig.ts
@@ -1,8 +1,11 @@
 import admin from 'firebase-admin';
 import { getFirestore } from 'firebase-admin/firestore';
 
-const serviceAccount = require('./serviceAccountKey.json'); 
-
+const serviceAccountString = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
+if (!serviceAccountString) {
+  console.error("FIREBASE_SERVICE_ACCOUNT_KEY is not set in the environment variables.");
+}
+const serviceAccount = JSON.parse(serviceAccountString!);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
